2d11699abc34da074b2dee8d1a91b3e7bcb13ff2
smgladkovskiy <SMGladkovskiy@gmail.com>
2011-06-17 13:03:47 +0400

    mysql schema correcting

--- pages_schema_mysql.sql
+++ pages_schema_mysql.sql
@@ -14,31 +14,41 @@ DEFAULT CHARSET=utf8
 COMMENT='System languages';
 
 -- -----------------------------------------------------
+-- Table `page_content_types`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `page_types` (
+  `id` TINYINT(2) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(128) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='Page types';
+
+-- -----------------------------------------------------
 -- Table `pages`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `pages` (
   `id` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `type_id` TINYINT(2) UNSIGNED NOT NULL ,
   `parent_id` SMALLINT(5) UNSIGNED NULL DEFAULT NULL ,
-  `alias` VARCHAR(64) NOT NULL ,
+  `alias` VARCHAR(64) NULL DEFAULT NULL ,
   `date_create` INT(10) UNSIGNED NOT NULL ,
   `is_active` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' ,
-  `lft` MEDIUMINT NOT NULL ,
-  `rgt` MEDIUMINT NOT NULL ,
-  `lvl` MEDIUMINT NOT NULL ,
-  `scope` SMALLINT NOT NULL ,
+  `lft` MEDIUMINT NULL DEFAULT NULL ,
+  `rgt` MEDIUMINT NULL DEFAULT NULL ,
+  `lvl` MEDIUMINT NULL DEFAULT NULL ,
+  `scope` SMALLINT NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
-  INDEX `fk_page_parent` (`parent_id` ASC) )
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `page_content_types`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `page_content_types` (
-  `id` TINYINT(2) UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB;
+  INDEX `fk_page_parent` (`parent_id` ASC) ,
+  INDEX `fk_page_type` (`type_id` ASC) ,
+  CONSTRAINT `fk_page_type`
+    FOREIGN KEY (`type_id` )
+    REFERENCES `page_types` (`id` )
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='Pages table';
 
 
 -- -----------------------------------------------------
@@ -46,7 +56,6 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `page_content` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `type_id` TINYINT(2) UNSIGNED NOT NULL ,
   `page_id` SMALLINT(5) UNSIGNED NULL DEFAULT NULL ,
   `lang_id` TINYINT(2) UNSIGNED NOT NULL ,
   `title` VARCHAR(256) NOT NULL ,
@@ -55,7 +64,6 @@ CREATE  TABLE IF NOT EXISTS `page_content` (
   PRIMARY KEY (`id`) ,
   INDEX `fk_page-content_page` (`page_id` ASC) ,
   INDEX `fk_page-content_system-language` (`lang_id` ASC) ,
-  INDEX `fk_page-content_type` (`type_id` ASC) ,
   CONSTRAINT `fk_page-content_page`
     FOREIGN KEY (`page_id` )
     REFERENCES `pages` (`id` )
@@ -65,10 +73,7 @@ CREATE  TABLE IF NOT EXISTS `page_content` (
     FOREIGN KEY (`lang_id` )
     REFERENCES `system_languages` (`id` )
     ON DELETE RESTRICT
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_page-content_type`
-    FOREIGN KEY (`type_id` )
-    REFERENCES `page_content_types` (`id` )
-    ON DELETE RESTRICT
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARSET=utf8
+COMMENT='Pages content table';
